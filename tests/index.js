1111b192bbfdcffd682c9c86a8f9d79255492ae9
merapi <marcin@webskill.pl>
2022-01-11 22:01:19 +0100

    GPU Info

--- tests/index.js
+++ tests/index.js
@@ -6,7 +6,7 @@ try {
             const browser = await playwright[browserType].launch();
             const context = await browser.newContext();
             const page = await context.newPage();
-            await page.goto('http://whatsmyuseragent.org/');
+            await page.goto('about:gpu');
             await page.screenshot({path: `./artifacts/example-${browserType}.png`});
             await browser.close();
         }
