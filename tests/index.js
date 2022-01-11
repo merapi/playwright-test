df05189afea11078c0659d79e174114115c8ce79
merapi <marcin@webskill.pl>
2022-01-11 22:10:29 +0100

    Test only Chromium

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,7 @@ const playwright = require('playwright');
 
 try {
     (async () => {
-        for (const browserType of ['chromium', 'firefox', 'webkit']) {
+        for (const browserType of ['chromium']) { //, 'firefox', 'webkit'
             const browser = await playwright[browserType].launch();
             const context = await browser.newContext();
             const page = await context.newPage();
